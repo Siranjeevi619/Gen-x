ff47ab0c339a7b5c45533781be8df82229ccd618
Siranjeevi619 <siranjeevi0619@gmail.com>
2026-09-25 20:15:13 +0530

    fix: added logic system messages

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import streamlit as st
 from dotenv import load_dotenv
 from langchain_groq import ChatGroq
 from langchain_core.messages import HumanMessage , AIMessage
+from langchain_core.prompts import ChatPromptTemplate
 
 
 load_dotenv()
@@ -14,6 +15,14 @@ model = ChatGroq(model='openai/gpt-oss-20b', temperature=0.7, api_key=groq_api_k
 
 st.title("GEX")
 
+prompt = ChatPromptTemplate.from_messages([
+    ("system",
+    "You are a helpful programming tutor. Explain technical concepts simply and give examples. If user asked about any different topics like cooking, movies and other things. just return like invalid query"),
+    ("placeholder",
+    "{message}")
+])
+
+chain = prompt | model
 
 if "messages" not in st.session_state:
     st.session_state.messages = []
@@ -34,10 +43,15 @@ if input:
     st.session_state.messages.append(human_message)
     st.chat_message('user').write(input)
 
-    response = model.invoke(st.session_state.messages)
+    response = chain.invoke({
+        "message":st.session_state.messages
+    })
+
     print(response)
 
-    ai_message = AIMessage(content=response.content)
+    ai_message = AIMessage(
+        content=response.content
+        )
     st.session_state.messages.append(ai_message)
 
     st.chat_message('assistant').write(response.content)
