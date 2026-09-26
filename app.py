681933c84d2be6440e66eff28f7c8e9bfd14175e
Siranjeevi619 <siranjeevi0619@gmail.com>
2026-09-26 13:01:31 +0530

    feat: added redis memory

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ from langchain_core.messages import AIMessage, HumanMessage
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_groq import ChatGroq
+from langchain_redis import RedisChatMessageHistory
 
 load_dotenv()
 
@@ -25,7 +26,7 @@ if isclear:
 
 prompt = ChatPromptTemplate.from_messages([
     ("system",
-    "You are a helpful programming tutor. Explain technical concepts simply and give examples. If user asked about any different topics like cooking, movies and other things. just return like invalid query"),
+    "You are a helpful programming tutor Gex. Explain technical concepts simply and give examples. If user asked about any different topics like cooking, movies and other things. just reply like you can help with only about Technology"),
     ("placeholder","{message}")
 ])
 
@@ -33,13 +34,18 @@ parser = StrOutputParser()
 
 chain = prompt | model | parser
 
-if "history" not in st.session_state:
-    st.session_state.history = InMemoryChatMessageHistory()
+if "session_id" not in st.session_state:
+    st.session_state.session_id = "user_1"
 
-for message in st.session_state.history:
+history = RedisChatMessageHistory(
+    session_id = st.session_state.session_id,
+    redis_url="redis://localhost:6379"
+)
+
+for message in history.messages:
 
     if isinstance(message, HumanMessage):
-        st.chat_message('user').write(message.conent)
+        st.chat_message('user').write(message.content)
 
     if isinstance(message, AIMessage):
         st.chat_message('assistant').write(message.content)
@@ -50,20 +56,19 @@ input = st.chat_input("input")
 if input:
     human_message = HumanMessage(content=input)
 
-    st.session_state.history.add_message(human_message)
+    history.add_message(human_message)
 
     st.chat_message('user').write(input)
-
 
     with st.chat_message('assistant'):
         response = st.write_stream(
             chain.stream({
-                "message":st.session_state.history.messages
+                "message":history.messages
             })
         )
     
     ai_message = AIMessage(content=response)
-    st.session_state.history.add_message(ai_message)
+    history.add_message(ai_message)
 
 
 
